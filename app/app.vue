56057a0a8cd319c70ddd4fcc712e26e73b0b9549
kabix09 <kabix.009@gmail.com>
2026-08-13 19:38:18 +0200

    feat(design): add fonts list

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,9 @@
 <script setup>
+import '@fontsource/plus-jakarta-sans/800.css'; // Waga 800 dla nagłówków
+import '@fontsource/plus-jakarta-sans/700.css'; 
+import '@fontsource/dm-sans/400.css';         // Waga 400 dla tekstu
+import '@fontsource/dm-sans/500.css';
+import '@fontsource/jetbrains-mono/400.css';  // Mono
 </script>
 
 <template >
